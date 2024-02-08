2186c5f87a9350ad5c02789d54d68d64005f9583
jheller9 <jasonheller96@gmail.com>
2024-02-08 16:19:01 -0500

    format

--- public/app.js
+++ public/app.js
@@ -1,27 +1,30 @@
-function isInViewport(element) {
-	const rect = element.getBoundingClientRect();
+function setScrollCSS() {
+	const htmlElement = document.documentElement
+	const percentScrolled = htmlElement.scrollTop / (htmlElement.scrollHeight - htmlElement.clientHeight)
 
-	return (
-		rect.top > 0 &&
-		rect.bottom < window.innerHeight
-	);
+	htmlElement.style.setProperty("--scroll", Math.min(percentScrolled * 100 , 100))
+	console.log(Math.min(percentScrolled * 100 , 100))
 }
 
-const bodies = document.getElementsByClassName('body');
-
-document.addEventListener('scroll', function () {
-	for(body of bodies) {
-		if (isInViewport(body)) {
-			body.style.borderColor = "#707070";
-		} else {
-			body.style.borderColor  = "#404040";
-		}
+const observer = new IntersectionObserver(entries => {
+	for (let i = entries.length - 1; i >= 0; i--) {
+	  const entry = entries[i]
+	  if (entry.isIntersecting) {
+		document.querySelectorAll("[data-img]").forEach(img => {
+		  img.classList.remove("show")
+		})
+		const img = document.querySelector(entry.target.dataset.imgToShow)
+		img?.classList.add("show")
+		break
+	  }
 	}
-}, {
-	passive: true
-});
+  })
+  
+  document.querySelectorAll("[data-img-to-show]").forEach(section => {
+	observer.observe(section)
+  })
 
-const slideimages = [
+  const slideimages = [
 	createImgArray("media/projects/project3_gifA.gif", "media/projects/project3_gifB.gif", "media/projects/project3_gifC.gif"),
 	createImgArray("media/projects/project4_gifA.gif", "media/projects/project4_gifB.gif", "media/projects/project4_gifC.gif")
 ]
@@ -42,8 +45,6 @@ function slideshowStep() {
 	setTimeout("slideshowStep()", 2500)
 }
 
-slideshowStep()
-
 function createImgArray(...urls) {
 	var imgArray = new Array()
 	
@@ -54,3 +55,9 @@ function createImgArray(...urls) {
 
 	return imgArray
 }
+
+window.addEventListener("scroll", setScrollCSS)
+window.addEventListener("resize", setScrollCSS)
+
+setScrollCSS()
+slideshowStep()
